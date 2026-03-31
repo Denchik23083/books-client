047b5ce24681a36dcf3a6603588c6a313594d48d
Denchik23083 <deniskudravov228@gmail.com>
2026-03-31 12:18:31 +0200

    all admins is done

--- src/app/services/god-service.ts
+++ src/app/services/god-service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { UserResponce } from '../models/responces/user-responce.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GodService {}
+export class GodService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'https://localhost:7001/api/god';
+
+  getAllAdmins(): Observable<UserResponce[]> {
+    return this.http.get<UserResponce[]>(this.apiUrl);
+  }
+
+  getAdmin(id: number): Observable<UserResponce> {
+    return this.http.get<UserResponce>(`${this.apiUrl}/${id}`);
+  }
+}
